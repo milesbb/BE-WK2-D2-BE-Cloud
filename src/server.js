d520a147b234d62b3269615d2798e48096dd43a6
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-10-06 08:30:24 +0100

    fixed Cors

--- src/server.js
+++ src/server.js
@@ -6,11 +6,18 @@ import cors from "cors";
 const server = express();
 const port = 3001;
 
+server.use(
+    cors({
+      origin: "*",
+      methods: ["GET", "POST", "PUT", "DELETE"],
+    })
+  );
+
 server.use(express.json());
 
 server.use("/authors", authorsRouter);
 
-server.use(cors());
+
 
 server.listen(port, () => {
   console.table(listEndpoints(server));
