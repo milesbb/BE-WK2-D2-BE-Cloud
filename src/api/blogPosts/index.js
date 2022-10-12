b79f432d654288a5d488196c5e599d0205438316
Miles Bailey-Braendgaard <milesjbb@gmail.com>
2022-10-12 15:47:52 +0100

    changed pdf ep again

--- src/api/blogPosts/index.js
+++ src/api/blogPosts/index.js
@@ -46,10 +46,10 @@ blogPostsRouter.get("/:id/pdf", async (req, res, next) => {
 
     res.setHeader("Content-Disposition", `attachment; filename=blogPost${idParam}.pdf`);
 
-    // const source = await createBlogPostPdf(idParam);
-    // const destination = res;
-
-    pipeline(createBlogPostPdf(idParam), res, (error) => {
+    const source = await createBlogPostPdf(idParam);
+    const destination = res;
+    
+    pipeline(source, destination, (error) => {
       if (error) console.log(error);
     });
   } catch (error) {
